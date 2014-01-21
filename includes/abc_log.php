5a20a500434809bc72b73666051f0b6bb2a73288
正邪 <zhengxie.lj@taobao.com>
2014-01-21 13:33:23 +0800

    gitignore

--- includes/abc_log.php
+++ includes/abc_log.php
@@ -5,7 +5,7 @@
 */
 function abc_log($path, $message)
 {
-    $time = date('Y-m-d H:i:s');
+  $time = date('Y-m-d H:i:s');
 	$log = $time . " [file] " . $path . " [message] " . $message . PHP_EOL;
 	file_put_contents(ROOT_PATH . "includes/log.txt", $log, FILE_APPEND);
 }
