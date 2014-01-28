e71750d4e9e1c80bcb5e9b6f91fd78c584fa0d49
colorhook <colorhook@gmail.com>
2014-01-28 21:07:44 +0800

    add olereade

--- admin/Excel/reader.php
+++ admin/Excel/reader.php
@@ -28,7 +28,7 @@
 
 
 //require_once 'PEAR.php';
-require_once 'Spreadsheet/Excel/Reader/OLERead.php';
+require_once dirname(__FILE__) . '/OLERead.php';
 //require_once 'OLE.php';
 
 define('SPREADSHEET_EXCEL_READER_BIFF8',             0x600);
@@ -258,7 +258,7 @@ class Spreadsheet_Excel_Reader
      */ 
     function Spreadsheet_Excel_Reader()
     {
-        $this->_ole =& new OLERead();
+        $this->_ole = new OLERead();
         $this->setUTFEncoder('iconv');
     }
 
