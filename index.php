fef1b6a9ed48bf6d3495d61321dd13ecf0166918
正邪 <zhengxie.lj@taobao.com>
2014-02-06 23:46:51 +0800

    style

--- index.php
+++ index.php
@@ -138,7 +138,10 @@ if (!$smarty->is_cached('index.dwt', $cache_id))
         $ad = $db->getRow($sql, true);
         $smarty->assign('ad', $ad);
     }
-
+    /*
+    $flashdb = get_flash_xml();
+    $smarty->assign('ad_banner', $flashdb);
+    */
     /* links */
     $links = index_get_links();
     $smarty->assign('img_links',       $links['img']);
@@ -359,5 +362,27 @@ function index_get_links()
 
     return $links;
 }
+function get_flash_xml()
+{
+    $flashdb = array();
+    if (file_exists(ROOT_PATH . DATA_DIR . '/flash_data.xml'))
+    {
 
+        // 兼容v2.7.0及以前版本
+        if (!preg_match_all('/item_url="([^"]+)"\slink="([^"]+)"\stext="([^"]*)"\ssort="([^"]*)"/', file_get_contents(ROOT_PATH . DATA_DIR . '/flash_data.xml'), $t, PREG_SET_ORDER))
+        {
+            preg_match_all('/item_url="([^"]+)"\slink="([^"]+)"\stext="([^"]*)"/', file_get_contents(ROOT_PATH . DATA_DIR . '/flash_data.xml'), $t, PREG_SET_ORDER);
+        }
+
+        if (!empty($t))
+        {
+            foreach ($t as $key => $val)
+            {
+                $val[4] = isset($val[4]) ? $val[4] : 0;
+                $flashdb[] = array('src'=>$val[1],'url'=>$val[2],'text'=>$val[3],'sort'=>$val[4]);
+            }
+        }
+    }
+    return $flashdb;
+}
 ?>
