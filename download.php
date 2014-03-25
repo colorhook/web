24ef110a20d4b0510463510085a5abdac7c1e06c
colorhook <colorhook@gmail.com>
2014-03-25 13:33:22 +0800

    add download.php

--- download.php
+++ download.php
@@ -2,22 +2,33 @@
 define('IN_ECS', true);
 require(dirname(__FILE__) . '/includes/init.php');
 if (empty($_SESSION['user_id'])){
-  return ecs_header("Location: ./user.php\n");
+  ecs_header("Location: ./user.php\n");
+  return;
 }
 
-$file = $_REQUEST['file'];
-$file = preg_replace("/[\/\\]/", "", $file);
+$file = isset($_REQUEST['file']) ? $_REQUEST['file'] : "";
+
+if(!$file){
+ echo "File is required";
+ return;
+}
+$file = preg_replace("/\//", "", $file);
+$file = preg_replace("/\\\\/", "", $file);
 $file = ROOT_PATH . 'images/upload/File/' . $file;
 
-header('Content-Description: File Transfer');    
-header('Content-Type: application/octet-stream');    
-header('Content-Disposition: attachment; filename='.basename($file));
-header('Content-Transfer-Encoding: binary');    
-header('Expires: 0');    
-header('Cache-Control: must-revalidate, post-check=0, pre-check=0');    
-header('Pragma: public');    
-header('Content-Length: ' . filesize($file));    
-ob_clean();
-flush();    
-readfile($file);
+if (file_exists($file)){
+	header('Content-Description: File Transfer');    
+	header('Content-Type: application/octet-stream');    
+	header('Content-Disposition: attachment; filename='.basename($file));
+	header('Content-Transfer-Encoding: binary');    
+	header('Expires: 0');    
+	header('Cache-Control: must-revalidate, post-check=0, pre-check=0');    
+	header('Pragma: public');    
+	header('Content-Length: ' . filesize($file));    
+	ob_clean();
+	flush();    
+	readfile($file);
+}else{
+  echo "File: $file not found";
+}
 ?>
