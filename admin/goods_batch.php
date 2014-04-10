e1ce3634842d1adf0dc46f51c3c2917c0388052d
colorhook <colorhook@gmail.com>
2014-04-10 20:16:27 +0800

    strip tags

--- admin/goods_batch.php
+++ admin/goods_batch.php
@@ -361,13 +361,13 @@ elseif ($_REQUEST['act'] == 'upload')
             
             $arr['keywords'] = '';
             $arr['goods_brief'] = '';
-            $arr['goods_desc'] = strip_tags($line_list[24], '<span><b><p><div><i><s><table><tr><td><th><li><ul><ol><strong><u><img>');
+            $arr['goods_desc'] = htmlspecialchars($line_list[24]);
             
             /* hack start */
            // $arr['goods_desc'] = $line_list[24];
             /* hack end */
             
-            $arr['goods_desc'] = substr($arr['goods_desc'], 1, -1);
+           // $arr['goods_desc'] = substr($arr['goods_desc'], 1, -1);
             $arr['goods_number'] = $line_list[10];
             $arr['warn_number'] =1;
             $arr['is_best'] = 0;
